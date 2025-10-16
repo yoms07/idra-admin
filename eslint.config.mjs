99af3356e9c0836efb7121cba43e46c83df64fb3
Jason Stanley <dummy.jason00@gmail.com>
2025-10-16 15:33:35 +0700

    fix rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/no-unescaped-entities': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
