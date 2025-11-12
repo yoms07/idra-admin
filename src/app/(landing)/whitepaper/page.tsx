e14e4ff4d73c7ce62c2a40823acfc7d7ae6fe746
Jason Stanley <dummy.jason00@gmail.com>
2025-11-12 17:58:29 +0700

    add whitepaper

---
+++ src/app/(landing)/whitepaper/page.tsx
@@ -0,0 +1,27 @@
+import fs from "fs";
+import path from "path";
+import ReactMarkdown from "react-markdown";
+
+export const dynamic = "force-static";
+
+async function getMarkdownContent() {
+  const fileContent = fs.readFileSync(
+    "src/app/(landing)/whitepaper/whitepaper.md",
+    "utf-8"
+  );
+  return fileContent;
+}
+
+export default async function TermsPage() {
+  const markdownContent = await getMarkdownContent();
+
+  return (
+    <div className="bg-landing-background">
+      <div className="max-w-7xl mx-auto px-6 pb-8">
+        <div className="prose prose-gray max-w-none">
+          <ReactMarkdown>{markdownContent}</ReactMarkdown>
+        </div>
+      </div>
+    </div>
+  );
+}
